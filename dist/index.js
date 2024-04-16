4afb89bb53fed8f338e1578f7179f205d04a0e85
arun <arunkumara1b@gmail.com>
2024-04-16 11:35:41 +0530

    #test

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,14 @@ Object.defineProperty(exports, "Checking", {
     return _Checking.default;
   }
 });
+Object.defineProperty(exports, "NavBar", {
+  enumerable: true,
+  get: function () {
+    return _Navbar.NavBar;
+  }
+});
 var _Checking = _interopRequireDefault(require("./Checking"));
+var _Navbar = require("./NavBar/Navbar");
 require("./tailwind.css");
 var _Button = require("./Button");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
