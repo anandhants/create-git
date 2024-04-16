318b8d569b7a854b21344b10f1108a9290fa7e93
arun <arunkumara1b@gmail.com>
2024-04-16 15:32:30 +0530

    Test

--- dist/Checking.js
+++ dist/Checking.js
@@ -6,11 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 exports.Checking = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _core = require("@mantine/core");
+require("@mantine/core/styles.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Checking = () => {
   return /*#__PURE__*/_react.default.createElement(_core.MantineProvider, null, /*#__PURE__*/_react.default.createElement(_core.Title, {
     order: 1,
     c: "dimmed"
-  }, "ssssssssssssssssssss"), /*#__PURE__*/_react.default.createElement(_core.Button, null, "Verdana button"));
+  }, "Checking namre"), /*#__PURE__*/_react.default.createElement(_core.Button, null, "Verdana button"));
 };
 exports.Checking = Checking;
--- src/Components/Checking.tsx
+++ src/Components/Checking.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import { Button, MantineProvider, Title } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 export const Checking = () => {
     return (
         <MantineProvider>
-            <Title order={1} c={"dimmed"}>ssssssssssssssssssss</Title>
+            <Title order={1} c={"dimmed"}>Checking namre</Title>
             <Button>Verdana button</Button>
         </MantineProvider>
     )
