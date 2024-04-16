e80b370a6c16b2a2300cd6437595200a7db48f68
arun <arunkumara1b@gmail.com>
2024-04-16 11:41:03 +0530

    #test

--- dist/NavBar/Checking.js
+++ dist/NavBar/Checking.js
@@ -4,7 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.Checking = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Checking = () => {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, "check");
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, "check");
 };
 exports.Checking = Checking;
