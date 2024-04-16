447bd79afddcec5a7fab97d8611ab360b07737fb
arun <arunkumara1b@gmail.com>
2024-04-16 14:38:14 +0530

    #tests111

--- dist/Checking.js
+++ dist/Checking.js
@@ -4,9 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+var _core = require("@mantine/core");
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Checking = () => {
-  return /*#__PURE__*/_react.default.createElement("p", null, "ssssssssssssssssssss");
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_core.Title, {
+    order: 1,
+    c: "dimmed"
+  }, "ssssssssssssssssssss"), /*#__PURE__*/_react.default.createElement(_core.Button, null, "Verdana button"));
 };
 var _default = exports.default = Checking;
